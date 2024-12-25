7610e273645340f363903e1326e40b49c8371929
Peter Jiang <peter.j@usask.ca>
2024-12-25 07:42:07 -0500

    linked list constructor and node class

--- Leetcode Question/Python Data Structure & Algorithm Course/Linked List.py
+++ Leetcode Question/Python Data Structure & Algorithm Course/Linked List.py
@@ -1,3 +1,4 @@
+# Basic linked list structure
 head = {
     "value": 11,
     "next": {
@@ -5,11 +6,44 @@ head = {
         "next": {
             "value": 23,
             "next": {
-                "value":7,
+                "value": 7,
                 "next": None
             }
         }
     }
 }
+# To access 23
+# print(head["next"]["next"]["value"])
 
-print(head["next"]["next"]["value"])
+
+
+# only for creating a new node
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# Linked list constructor
+class LinkedList:
+    # create a new Node
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+my_linked_list = LinkedList(4)
+
+print(my_linked_list.head.value)
+
+    # # create a new node and add the node to end
+    # def append(self, value):
+    #
+    # # create a new node and the node to beginning
+    # def prepend(self, value):
+    #
+    # # create a new node and add the node to certain index
+    # def insert(self, index, value):
+
